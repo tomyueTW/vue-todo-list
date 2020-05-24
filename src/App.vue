0f0ffdcb57b411f265f005e11eaf72e0a11300c1
Tom <yue.kun.ting@gmail.com>
2020-05-24 14:18:32 +0800

    Feature - todo list

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Todos v-bind:todos="todos" v-on:delete-todo="deleteTodo"/>
+    <AddTodo v-on:add-todo="addTodo"/>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import Todos from './components/Todos';
+  import AddTodo from './components/AddTodo';
+  export default {
+    name: 'app',
+    components: {
+      Todos,
+      AddTodo
+    },
+    data() {
+      return {
+        todos: [
+          {
+            id: 1,
+            title: '起床',
+            completed: false
+          },
+          {
+            id: 2,
+            title: '騎車',
+            completed: false
+          },
+          {
+            id: 3,
+            title: '上班',
+            completed: false
+          },
+          {
+            id: 4,
+            title: '回家',
+            completed: false
+          },
+          {
+            id: 5,
+            title: '晚餐',
+            completed: false
+          }
+        ],
+      }
+    },
+    methods: {
+      addTodo(newTodoObj) {
+        this.todos = [...this.todos, newTodoObj];
+      },
+      deleteTodo(todoId) {
+        this.todos = this.todos.filter(todo => todo.id !== todoId);
+      }
+    }
   }
-}
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <form @submit="addTodo">
+            <input type="text" v-model="title" name="title">
+            <button type="submit">Add</button>
+        </form>
+    </div>
+</template>
+<script>
+    import { v4 as uuidv4 } from 'uuid';
+    export default {
+        name: 'AddTodo',
+        data() {
+            return {
+                title: ''
+            }
+        },
+        methods: {
+            addTodo(e) {
+                e.preventDefault();
+                const newTodoObj = {
+                    id: uuidv4(),
+                    title: this.title,
+                    completed: false
+                }
+                this.$emit('add-todo', newTodoObj);
+                this.title = '';
+            }
+        }
+    }
+</script>
+<style scoped>
+</style>
